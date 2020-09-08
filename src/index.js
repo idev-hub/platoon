2601ab2569eb22650e735430b70a98dc1818cfcb
idev-hub <7vg7tek7sG>
2020-09-09 04:55:54 +0500

    new

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ window.addEventListener("DOMContentLoaded", function (e) {
         for (let i = 0; i < items.length; i++) {
             const item = items[i]
 
-            item.addEventListener("click", function (e){
+            item.addEventListener("click", function (e) {
                 e.preventDefault();
                 const content = item.getElementsByClassName("modal-content")[0].innerHTML
 
@@ -44,7 +44,7 @@ window.addEventListener("DOMContentLoaded", function (e) {
                 const images = imgs.getElementsByTagName("img")
 
                 for (let j = 0; j < images.length; j++) {
-                    images[j].addEventListener("click", function (e){
+                    images[j].addEventListener("click", function (e) {
                         e.preventDefault();
                         gallary.getElementsByTagName("img")[0].setAttribute("src", images[j].getAttribute("src"))
                     })
@@ -55,50 +55,136 @@ window.addEventListener("DOMContentLoaded", function (e) {
         }
     }
 
-
-    new Glide('.glide-2', {
+    const glide2 = new Glide('.glide-2', {
         type: 'slider',
         startAt: 0,
         gap: 10,
         perView: 1,
         bound: false,
         rewind: false,
-    }).mount()
+        dots: '.dots',
+    })
+    const glide2Container = document.getElementsByClassName("glide-2")[0]
+    if (glide2Container) {
+        const slides = glide2Container.getElementsByClassName("glide__slide")
+        const dots = glide2Container.getElementsByClassName("glide__bullets")[0]
 
-    new Glide('.glide-3', {
+        if (dots && slides.length > 0) {
+            dots.innerHTML = ""
+            for (let i = 0; i < slides.length; i++) {
+                let el = document.createElement("button")
+                el.classList.add("glide__bullet")
+                el.setAttribute("data-glide-dir", `=${i}`)
+                dots.append(el)
+            }
+        }
+    }
+    glide2.mount()
+
+    const glide3 = new Glide('.glide-3', {
         type: 'slider',
         startAt: 0,
         gap: 10,
         perView: 4,
         bound: false,
         rewind: false,
-    }).mount()
+    })
+    const glide3Container = document.getElementsByClassName("glide-3")[0]
+    if (glide3Container) {
+        const slides = glide3Container.getElementsByClassName("glide__slide")
+        const dots = glide3Container.getElementsByClassName("glide__bullets")[0]
+        let int = slides.length / glide3.settings.perView
 
-    new Glide('.glide-4', {
+        if (dots && int > 0) {
+            dots.innerHTML = ""
+            for (let i = 0; i < int; i++) {
+                let el = document.createElement("button")
+                el.classList.add("glide__bullet")
+                el.setAttribute("data-glide-dir", `=${i}`)
+                dots.append(el)
+            }
+        }
+    }
+    glide3.mount()
+
+    const glide4 = new Glide('.glide-4', {
         type: 'carusel',
         startAt: 0,
         gap: 30,
         perView: 6,
-    }).mount()
+    })
+    const glide4Container = document.getElementsByClassName("glide-4")[0]
+    if (glide4Container) {
+        const slides = glide3Container.getElementsByClassName("glide__slide")
+        const dots = glide3Container.getElementsByClassName("glide__bullets")[0]
+        let int = slides.length / glide3.settings.perView
+
+        if (dots && int > 0) {
+            dots.innerHTML = ""
+            for (let i = 0; i < int; i++) {
+                let el = document.createElement("button")
+                el.classList.add("glide__bullet")
+                el.setAttribute("data-glide-dir", `=${i}`)
+                dots.append(el)
+            }
+        }
+    }
+    glide4.mount()
+
 
     if (window.innerWidth >= 787) {
-        new Glide('.glide', {
+        const glide = new Glide('.glide', {
             type: 'slider',
             startAt: 0,
             gap: 10,
             perView: 3,
             bound: false,
             rewind: false,
-        }).mount()
+        })
+        const glideContainer = document.getElementsByClassName("glide")[0]
+        if (glideContainer) {
+            const slides = glide3Container.getElementsByClassName("glide__slide")
+            const dots = glide3Container.getElementsByClassName("glide__bullets")[0]
+            let int = slides.length / glide3.settings.perView
+
+            if (dots && int > 0) {
+                dots.innerHTML = ""
+                for (let i = 0; i < int; i++) {
+                    let el = document.createElement("button")
+                    el.classList.add("glide__bullet")
+                    el.setAttribute("data-glide-dir", `=${i}`)
+                    dots.append(el)
+                }
+            }
+        }
+        glide.mount()
+
     } else {
-        new Glide('.glide', {
+        const glide = new Glide('.glide', {
             type: 'slider',
             startAt: 0,
             gap: 10,
             perView: 1,
             bound: false,
             rewind: false,
-        }).mount()
+        })
+        const glideContainer = document.getElementsByClassName("glide")[0]
+        if (glideContainer) {
+            const slides = glide3Container.getElementsByClassName("glide__slide")
+            const dots = glide3Container.getElementsByClassName("glide__bullets")[0]
+            let int = slides.length / glide3.settings.perView
+
+            if (dots && int > 0) {
+                dots.innerHTML = ""
+                for (let i = 0; i < int; i++) {
+                    let el = document.createElement("button")
+                    el.classList.add("glide__bullet")
+                    el.setAttribute("data-glide-dir", `=${i}`)
+                    dots.append(el)
+                }
+            }
+        }
+        glide.mount()
     }
 })
 
