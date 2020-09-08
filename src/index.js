067067934b214258a6a0f4b5d6bb9905d673ecf0
idev-hub <7vg7tek7sG>
2020-09-09 02:52:10 +0500

    new

--- src/index.js
+++ src/index.js
@@ -20,9 +20,39 @@ window.addEventListener("DOMContentLoaded", function (e) {
     const myLazyLoad = new LazyLoad()
     myLazyLoad.update()
 
-    if(window.innerWidth >= 787){
+    if (window.innerWidth >= 787) {
         const slidesContainer = document.getElementById("slide-page")
         const slider = new Slider(slidesContainer, 'slide')
+    }
+
+
+    const catalog = document.getElementsByClassName("catalog")[0]
+    if (catalog) {
+        const items = catalog.getElementsByClassName("_item")
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i]
+
+            item.addEventListener("click", function (e){
+                e.preventDefault();
+                const content = item.getElementsByClassName("modal-content")[0].innerHTML
+
+                const modal = document.getElementById("modal-3")
+                modal.getElementsByClassName("modal__content")[0].innerHTML = content
+
+                const gallary = modal.getElementsByClassName("gallary")[0]
+                const imgs = gallary.getElementsByClassName("_imgs")[0]
+                const images = imgs.getElementsByTagName("img")
+
+                for (let j = 0; j < images.length; j++) {
+                    images[j].addEventListener("click", function (e){
+                        e.preventDefault();
+                        gallary.getElementsByTagName("img")[0].setAttribute("src", images[j].getAttribute("src"))
+                    })
+                }
+
+                MicroModal.show("modal-3", {})
+            })
+        }
     }
 
 
@@ -47,11 +77,11 @@ window.addEventListener("DOMContentLoaded", function (e) {
     new Glide('.glide-4', {
         type: 'carusel',
         startAt: 0,
-        gap:  30,
+        gap: 30,
         perView: 6,
     }).mount()
 
-    if(window.innerWidth >= 787){
+    if (window.innerWidth >= 787) {
         new Glide('.glide', {
             type: 'slider',
             startAt: 0,
